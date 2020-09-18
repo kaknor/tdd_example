125ca478a349a98edcc3e3469f12c418c08353f0
mortician <mortician@morticians-MacBook-Air.local>
2020-09-18 12:43:20 +0200

    [PART I][4] Make amount private

--- src/money.py
+++ src/money.py
@@ -3,14 +3,14 @@ from typing import Type
 
 class Dollar:
     def __init__(self, amount) -> None:
-        self.amount = amount
+        self._amount = amount
 
     # As we are using Python3 no need to implement __ne__ as __ne__ is calling __eq__.
     def __eq__(self, o: object) -> bool:
         if isinstance(o, self.__class__):
-            return o.amount == self.amount
+            return o._amount == self._amount
         return False
 
 
     def times(self, multiplier: int ):
-        return Dollar(multiplier * self.amount)
+        return Dollar(multiplier * self._amount)
--- tests/test_money.py
+++ tests/test_money.py
@@ -6,12 +6,9 @@ from src.money import Dollar
 
 def test_multiplication():
     five = Dollar(5)
-    product = five.times(2)
-    assert 10 == product.amount
-    product = five.times(3)
-    assert 15 == product.amount
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3)
 
 def test_equality():
     assert Dollar(5) == Dollar(5)
     assert Dollar(5) != Dollar(6)
-    
