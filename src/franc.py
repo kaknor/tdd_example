d6434f2cbf64891d7dbee8c65327e979df849201
mortician <mortician@morticians-MacBook-Air.local>
2020-09-21 11:45:41 +0200

    [PART I][7] Change eq to verify class and amount.

--- src/franc.py
+++ src/franc.py
@@ -4,12 +4,5 @@ class Franc(Money):
     def __init__(self, amount) -> None:
         super(Franc, self).__init__(amount)
 
-    # As we are using Python3 no need to implement __ne__ as __ne__ is calling __eq__.
-    def __eq__(self, o: object) -> bool:
-        if isinstance(o, self.__class__):
-            return o._amount == self._amount
-        return False
-
-
-    def times(self, multiplier: int ):
+    def times(self, multiplier: int):
         return Franc(multiplier * self._amount)
